c4ae7cad68f7fe40658388151079872963c2ab54
teo <teo.sivanich08@gmail.com>
2026-04-30 22:51:59 -0500

    add ssl

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const app = express();
 const path = require('path');
-const port = 80;
+
+const privateKey = fs.readFileSync('key.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8');
+const passphrase = 'hi';
+const credentials = {key: privateKey, passphrase, cert: certificate};
+
+const httpsServer = https.createServer(credentials, app);
+
+function ensureSecure(req, res, next) {
+	if (req.secure) {
+		return next();
+	}
+	res.redirect('https://' + req.hostname + req.originalUrl);
+}
+
+app.use(ensureSecure);
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'src/index.html'));
-	//res.send('wassup');
 });
 
-app.listen(port, () => {
+app.listen(443, () => {
 	console.log('ts is running');
 });
